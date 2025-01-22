507a492b989c5eb26444d14fa605c2e0acba5e7c
Alasdair Warwick <54263156+rmgpanw@users.noreply.github.com>
2025-01-22 21:23:40 +0000

    add logistic regression model and update plot with discrimination line

--- iris_r/iris.R
+++ iris_r/iris.R
@@ -13,3 +13,19 @@ ggplot(iris, aes(x = Petal.Length, y = Petal.Width, color = Species)) +
        x = "Petal Length",
        y = "Petal Width") +
   theme_minimal()
+
+# logistic regression model to discriminate between virginica species and non-virginica
+iris$IsVirginica <- ifelse(iris$Species == "virginica", 1, 0)
+logreg_model <- glm(IsVirginica ~ Petal.Length + Petal.Width, data = iris, family = binomial)
+
+# Re-do earlier plot, now with linear discrimination line from logreg model
+ggplot(iris, aes(x = Petal.Length, y = Petal.Width, color = Species)) +
+  geom_point() +
+  labs(title = "Petal Length vs Petal Width by Species",
+       x = "Petal Length",
+       y = "Petal Width") +
+  theme_minimal() +
+  geom_abline(intercept = -coef(logreg_model)[1] / coef(logreg_model)[3], 
+              slope = -coef(logreg_model)[2] / coef(logreg_model)[3], 
+              color = "black", linetype = "dashed")
+
